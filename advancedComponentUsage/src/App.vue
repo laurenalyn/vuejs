f010199cbdad44ae00b98a423775cc6a29267163
laurenalyn <laurenalynwilkerson@gmail.com>
2019-04-28 19:42:57 -0400

    examples of advaced component usage

---
+++ advancedComponentUsage/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent= 'appQuote'">Quote</button>
+                <button @click="selectedComponent= 'appAuthor'">Author</button>
+                <button @click="selectedComponent= 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>default content</p>
+                    </component>
+                </keep-alive>
+                <!-- <appQuote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>An awesome quote!</p>
+                </appQuote> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/quote.vue';
+    import Author from './components/author.vue';
+    import New from './components/new.vue';
+
+    export default {
+        data: function () {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+    }
+</script>
+
+<style>
+</style>
