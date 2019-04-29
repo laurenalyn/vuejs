145f9c08b61fbcf4bda40c8adaa2033ebc03e5fb
laurenalyn <laurenalynwilkerson@gmail.com>
2019-04-29 16:56:42 -0400

    implements heal function

--- project1/app.js
+++ project1/app.js
@@ -38,7 +38,12 @@ new Vue ({
         },
 
         heal: function() {
-
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            }else {
+                this.playerHealth = 100;
+            }
+            this.monsterAttacks();
         },
 
         giveUp: function() {
