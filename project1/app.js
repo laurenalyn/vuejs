165e1683c1ace7bea507aab35b511c63c2791fd3
laurenalyn <laurenalynwilkerson@gmail.com>
2019-04-29 17:15:28 -0400

    adds log to specialAttack and heal buttons

--- project1/app.js
+++ project1/app.js
@@ -13,6 +13,7 @@ new Vue ({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
 
         attack: function() {
@@ -35,7 +36,12 @@ new Vue ({
         },
 
         specialAttack: function() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
             if(this.checkWin()) {
                 return;
             }
@@ -49,6 +55,11 @@ new Vue ({
             }else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
+            
             this.monsterAttacks();
         },
 
@@ -65,7 +76,7 @@ new Vue ({
             var damage = this.calculateDamage(5, 12);
             this.playerHealth -= damage;
             this.checkWin();
-            this.turns.upshift({
+            this.turns.unshift({
                 isPlayer: false,
                 text: 'Monster hits player for ' + damage
             });
